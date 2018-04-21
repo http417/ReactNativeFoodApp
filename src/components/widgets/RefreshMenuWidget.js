58018d475444331b1dfc7082c2f7061425ec6e48
Christopher Cheng <chris.h.cheng@gmail.com>
2018-04-20 23:07:36 -0400

    put in a new check after eech time the menu data is refreshed from the server.  remove any items from the cart that are no longer available in the menu.  note: a check is done every 10 minutes.

--- src/components/widgets/RefreshMenuWidget.js
+++ src/components/widgets/RefreshMenuWidget.js
@@ -6,31 +6,32 @@ import dataManager from '../../tools/dataFetch';
 export default class extends React.Component {
   constructor(props) {
     super(props);
-    if (this._menuDataStale()) { // check if the data is over 10 minutes old
+    if (this._menuDataStale(10)) { // check if the data is over 10 minutes old
       this._refreshStore(); // automatically fetch new data
     }
   }
 
-  _menuDataStale() {
+  _menuDataStale(maxMinutes) {
     const lastRefreshDate = this.props.lastRefreshDate || 0;
     const minutesSinceLastRefresh =
       Math.floor(((Math.abs(Date.now() - lastRefreshDate)) / 1000) / 60);
-    // console.log('minutes since last refresh: ', minutesSinceLastRefresh);
-    if (minutesSinceLastRefresh > 10) {
-      return true;
-    }
-    return false;
+    return (minutesSinceLastRefresh > maxMinutes);
   }
 
+  _isNewStoreDataValid = newStoreData => (
+    // greedy check, just make sure that there is data
+    Object.keys(newStoreData.mainItems).length &&
+          Object.keys(newStoreData.rawCategoryData).length
+  )
 
   _refreshStore() {
     // console.log('Updating the redux store');
     dataManager.processData
-      .then((storeData) => {
-        if (Object.keys(storeData.mainItems).length &&
-          Object.keys(storeData.rawCategoryData).length) {
-          this.props.refreshMenu(storeData);
-        } else {
+      .then((newStoreData) => {
+        if (this._isNewStoreDataValid) {
+          this._removeAnyDiscontinuedItems(newStoreData);
+          this.props.refreshMenu(newStoreData);
+        } else { // don't update the redux store; use old data
           throw new Error('Server returned missing data, so using old data');
         }
       })
@@ -40,7 +41,7 @@ export default class extends React.Component {
   }
 
   // if any items are no longer available (after a refresh), remove them from the cart
-  _removeDiscontinuedItems(newStoreData) {
+  _removeAnyDiscontinuedItems(newStoreData) {
     const { cart } = this.props; // { {itemID: quantity}, }
     const idsToRemove = []; // store id's of items that should be deleted after processing the data
     Object.keys(cart).forEach((itemId) => {
