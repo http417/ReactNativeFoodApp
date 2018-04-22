707257a5737b8a6f43a2724e92bca3f9d833ba72
Christopher Cheng <chris.h.cheng@gmail.com>
2018-04-22 17:47:07 -0400

    start of testing

--- App.test.js
+++ App.test.js
@@ -1,9 +1,11 @@
+import renderer from 'react-test-renderer';
+
 import React from 'react';
 import App from './App';
 
-import renderer from 'react-test-renderer';
-
 it('renders without crashing', () => {
+  const hello = 5;
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
+  expect(hello).toEqual(4);
 });
