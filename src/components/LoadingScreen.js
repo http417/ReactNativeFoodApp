e2198145aa31f6895ba7947855fe1a37cefcbbd3
Christopher Cheng <chris.h.cheng@gmail.com>
2018-04-22 09:43:43 -0400

    branded logo screen with black background and image logo

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { View, ActivityIndicator, StyleSheet } from 'react-native';
+import { View, ActivityIndicator, StyleSheet, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: 'slateblue',
+    backgroundColor: 'black',
     justifyContent: 'center',
+  },
+  headerImage: {
+    height: 60,
+    width: 140,
+    marginBottom: 20,
   },
 });
 
 // consider adding the refresh widget to this screen for a refresh check
 export default () => (
   <View style={styles.screen}>
+    <Image
+      style={styles.headerImage}
+      source={require('./widgets/logo2.png')} // eslint-disable-line
+    />
     <ActivityIndicator size="large" color="#0000ff" />
   </View>
 );
